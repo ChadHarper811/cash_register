673fcad74f8fd9b2f3d348d260806f2586d77945
Chad Harper <chadharper811@gmail.com>
2024-11-03 19:00:24 -0600

    added cid values to currency total displays

--- script.js
+++ script.js
@@ -14,7 +14,30 @@ let cid = [
 const displayTotal = document.getElementById("total");
 const totalCash = document.getElementById("cash");
 const changeDueDisplay = document.getElementById("change-due");
-const penniesTotal = document.getElementById()
+const penniesTotal = document.getElementById("pennies-total");
+const nickelsTotal = document.getElementById("nickels-total");
+const dimesTotal = document.getElementById("dimes-total");
+const qurtersTotal = document.getElementById("quarters-total");
+const onesTotal = document.getElementById("ones-total");
+const fivesTotal = document.getElementById("fives-total");
+const tensTotal = document.getElementById("tens-total");
+const twentiesTotal = document.getElementById("twenties-total");
+const hundredsTotal = document.getElementById("hundreds-total");
 
-displayTotal.innerText += ` $${price}`
 
+displayTotal.innerText += ` $${price}`;
+
+const updateValues = change => {
+    totalCash.value = "";
+}
+
+
+penniesTotal.innerText = `$${cid[0][1]}`;
+nickelsTotal.innerText = `$${cid[1][1]}`;
+dimesTotal.innerText = `$${cid[2][1]}`;
+qurtersTotal.innerText = `$${cid[3][1]}`;
+onesTotal.innerText = `$${cid[4][1]}`;
+fivesTotal.innerText = `$${cid[5][1]}`;
+tensTotal.innerText = `$${cid[6][1]}`;
+twentiesTotal.innerText = `$${cid[7][1]}`;
+hundredsTotal.innerText = `$${cid[8][1]}`;

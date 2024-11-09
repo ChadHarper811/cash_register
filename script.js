9ea0a1008d38908ff94f721033b9d67eb1860a43
Chad Harper <chadharper811@gmail.com>
2024-11-09 12:01:04 -0600

    minor changes to style and added while loop, need to fix infinite looping

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ const cashRegisterChange = () => {
   const priceTotal = Math.round(price * 100);
   const cashTotal = Math.round(Number(cashInput.value) * 100);
   const changeDue = cashTotal - priceTotal;
+  let change = changeDue;
   let registerDrawerTotal = 0;
   let descendingCid = cid.toReversed();
 
@@ -52,8 +53,38 @@ const cashRegisterChange = () => {
   } else if (registerDrawerTotal === changeDue) {
     changeDueDisplay("CLOSED", descendingCid)
   } else {
-    
-    changeDueDisplay("OPEN", descendingCid)
+    while (change >= 0) {
+      if (change > 10000) {
+        change -= 10000;
+        cid[8][1] -= 100
+      } else if ( change >= 2000) {
+        change -= 2000
+        cid[7][1] -= 20
+      } else if ( change >= 1000) {
+        change -= 1000
+        cid[6][1] -= 10
+      } else if ( change >= 500) {
+        change -= 500
+        cid[5][1] -= 5
+      } else if ( change >= 100) {
+        change -= 100
+        cid[4][1] -= 1
+      } else if ( change >= 25) {
+        change -= 25
+        cid[3][1] -= .25
+      } else if ( change >= 10) {
+        change -= 10
+        cid[3][1] -= .1
+      } else if ( change >= 5) {
+        change -= 5
+        cid[3][1] -= .05
+      } else if ( change >= 1) {
+        change -= 1
+        cid[3][1] -= .01
+      } else {
+        changeDueDisplay("OPEN", descendingCid)
+      }
+    }
   }
 
   updateValues();

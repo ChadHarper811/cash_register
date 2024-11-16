1187f6df6fc862c2449552f9b8247cbcac139a82
Chad Harper <chadharper811@gmail.com>
2024-11-16 15:21:52 -0600

    finished project and added notes to code for references

--- script.js
+++ script.js
@@ -37,10 +37,8 @@ const cashRegisterChange = () => {
   const changeDue = cashTotal - priceTotal;
   let change = changeDue;
   let registerDrawerTotal = 0;
-  let descendingCid = cid.toReversed();
 
   let cidChange = cid.map(ar => ar.slice())
-
   
   let changeDisplay = [
     ["ONE HUNDRED", 0],
@@ -66,55 +64,74 @@ const cashRegisterChange = () => {
     changeDueText.innerHTML = "No change due - customer paid with exact cash";
     return;
   } else if  (registerDrawerTotal < changeDue) {
-    changeDueDisplay("INSUFFICIENT_FUNDS", descendingCid)
-  } else if (registerDrawerTotal === changeDue) {
-    changeDueDisplay("CLOSED", descendingCid)
+    changeDueText.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`
   } else {
-    
+    // loop checks for highest amount down to lowest and subtracts from change and the cidChange to keep track. If no remainder the cid updates to match cidChange and updates all values then displays the change and status. Using all values *100 to avoid decimals.
     while (change >= 0 ) {
       if (change > 10000 && cidChange[8][1] > 0) {
         change -= 10000
+        // change to "ONE HUNDRED" value in cidChange
         cidChange[8][1] = Math.round((cidChange[8][1] * 100 - 10000)) / 100;
+        // Adding 1 unit to "ONE HUNDRED" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "ONE HUNDRED")][1] += 10000;
       } else if (change >= 2000 && cidChange[7][1] > 0) {
         change -= 2000
+        // change to "TWENTY" value in cidChange
         cidChange[7][1] = Math.round((cidChange[7][1] * 100 - 2000)) / 100;
+        // Adding 1 unit to "TWENTY" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "TWENTY")][1] += 2000;
       } else if ( change >= 1000 && cidChange[6][1] > 0) {
         change -= 1000
+        // change to "TEN" value in cidChange
         cidChange[6][1] = Math.round((cidChange[6][1] * 100 - 1000)) / 100;
+        // Adding 1 unit to "TEN" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "TEN")][1] += 1000;
       } else if ( change >= 500 && cidChange[5][1] > 0) {
         change -= 500
+        // change to "FIVE" value in cidChange
         cidChange[5][1] = Math.round((cidChange[5][1] * 100 - 500)) / 100;
+        // Adding 1 unit to "FIVE" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "FIVE")][1] += 500;
       } else if ( change >= 100 && cidChange[4][1] > 0) {
         change -= 100
+        // change to "ONE" value in cidChange
         cidChange[4][1] = Math.round((cidChange[4][1] * 100 - 100)) / 100;
+        // Adding 1 unit to "ONE" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "ONE")][1] += 100;
       } else if ( change >= 25 && cidChange[3][1] > 0) {
         change -= 25
+        // change to "QUARTER" value in cidChange
         cidChange[3][1] = Math.round((cidChange[3][1] * 100 - 25)) / 100;
+        // Adding 1 unit to "QUARTER" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "QUARTER")][1] += 25;
       } else if ( change >= 10 && cidChange[2][1] > 0) {
         change -= 10
+        // change to "DIME" value in cidChange
         cidChange[2][1] = Math.round((cidChange[2][1] * 100 - 10)) / 100;
+        // Adding 1 unit to "DIME" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "DIME")][1] += 10;
       } else if ( change >= 5 && cidChange[1][1] > 0) {
         change -= 5
+        // change to "NICKEL" value in cidChange
         cidChange[1][1] = Math.round((cidChange[1][1] * 100 - 5)) / 100;
+        // Adding 1 unit to "NICKEL" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "NICKEL")][1] += 5
       } else if ( change >= 1 && cidChange[0][1] > 0) {
         change -= 1
+        // change to "PENNY" value in cidChange
         cidChange[0][1] = Math.round((cidChange[0][1] * 100 - 1)) / 100;
+        // Adding 1 unit to "PENNY" value in changeDisplay
         changeDisplay[changeDisplay.findIndex(ar => ar[0] === "PENNY")][1] += 1
       } else { 
-        console.log(changeDisplay)
         if (change > 0) {
           changeDueText.innerHTML = `<p>Status: INSUFFICIENT_FUNDS</p>`
           updateValues()
+          // if change has remainer the cidChange reverts back to last cid
           cidChange = cid.map(ar => ar.slice());
+        } else if (registerDrawerTotal === changeDue) {
+          changeDueDisplay("CLOSED", changeDisplay)
         } else {
+          // if change has no remainder the cid updates to match cidChange
           cid = cidChange.map(ar => ar.slice());
           updateValues()
           changeDueDisplay("OPEN", changeDisplay);
